a9ebf81c829c64214c7e0579d6b68e1ac570895f
wpomier <55899540+pomgui@users.noreply.github.com>
2023-06-11 20:13:49 -0300

    feat(deepMerge): Change the return value from any to generic <T>

--- src/deepmerge.ts
+++ src/deepmerge.ts
@@ -7,20 +7,20 @@
  * @param objects one or more objects to be merged with the target
  * @returns the modified target
  */
-export function deepMerge(target: any, ...objects: any[]): any {
+export function deepMerge<T>(target: T, ...objects: any[]): T {
   for (const source of objects) {
     if (target !== source)
       Object.entries(source).forEach(([key, sourceValue]) => {
-        let targetValue = target[key];
-        if (targetValue !== sourceValue || !target.hasOwnProperty(key)) {
+        let targetValue = (target as any)[key];
+        if (targetValue !== sourceValue || !(target as any).hasOwnProperty(key)) {
           if (typeof sourceValue === 'object' && sourceValue !== null) {
             if (Array.isArray(sourceValue) && !Array.isArray(targetValue))
-              targetValue = target[key] = [];
+              targetValue = (target as any)[key] = [];
             else if (typeof targetValue !== 'object')
-              targetValue = target[key] = {};
+              targetValue = (target as any)[key] = {};
             deepMerge(targetValue, sourceValue);
           } else if (targetValue === undefined || sourceValue !== undefined) {
-            target[key] = sourceValue;
+            (target as any)[key] = sourceValue;
           }
         }
       })
